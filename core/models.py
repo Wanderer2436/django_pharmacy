169c1f87767672ea5a721d81b40556be8ac51165
Wanderer2436 <timka2436@gmail.com>
2022-03-28 20:48:03 +0500

    #2 Дополнил и добавил новые модели

--- core/models.py
+++ core/models.py
@@ -11,11 +11,15 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField('Название товара', max_length=50, unique=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    type = models.CharField('Форма выпуска', max_length=50, blank=True)
     manufacturer = models.CharField('Производитель', max_length=20, blank=True, null=True)
     country = models.CharField('Страна производителя', max_length=15, blank=True, null=True)
     price = models.IntegerField('Цена товара', max_length=5)
-    description = models.TextField('Описание товара', max_length=250, blank=True, null=True)
     photo = models.ImageField('Изображение товара', upload_to='products/', blank=True)
+    indications_for_use = models.TextField('Показания к применению', blank=True)
+    mode_of_application = models.TextField('Способ применения', blank=True)
+    composition = models.TextField('Состав', blank=True)
+    contraindications = models.TextField('Противопоказания', blank=True)
 
     def __str__(self):
         return self.name
@@ -27,19 +31,22 @@ class Pharmacy(models.Model):
     house = models.IntegerField("Номер дома", blank=True, null=True)
     phone = models.IntegerField("Номер телефона", blank=True, null=True)
     photo = models.ImageField('Изображение аптеки', upload_to='pharmacies/', blank=True)
+    opening_time = models.TimeField('Время открытия', default='9:00', blank=True)
+    closing_time = models.TimeField('Время закрытия', default='21:00', blank=True)
 
     def __str__(self):
         return self.name
 
 
-class Supply(models.Model):
-    supplier = models.CharField('Название поставщика', max_length=25)
+class Available(models.Model):
     pharmacy = models.ForeignKey('Pharmacy', on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField('Количество')
 
     def __str__(self):
-        return f'{self.product} {self.quantity}'
+        return f'{self.pharmacy} {self.product} {self.quantity}'
 
 
-
+class ProductInOrder(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField('Количество товара', default=1)
